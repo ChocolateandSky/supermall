a85bfbf17f4145fa1b4eb1796660e74297c9d026
ChocolateandSky <1017192349@qq.com>
2020-08-15 16:28:06 +0800

    注释

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item , index) in titles"
+      :key="index"
+      class="tab-item"
+      :class="{Active:currentIndex===index}"
+      @click="itemClick(index)"
+    >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  methods:{
+    itemClick(index){
+      this.currentIndex = index
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  background-color: #fff;
+  font-size: 15px;
+  height: 44px;
+  line-height: 44px;
+  position: sticky;
+  top: 44px;
+}
+.tab-item {
+  flex: 1;
+}
+.tab-item span {
+    padding: 5px;
+}
+
+
+.Active {
+  color: var(--color-high-text);
+}
+.Active span{
+ border-bottom: 3px solid var(--color-tint);
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,78 @@
 <template>
-  <div id="Home">
+  <div id="home">
       <nav-bar  class="home-nav">
         <div slot="center">购物街</div>
       </nav-bar>
       <home-swiper :banners="banners"/>
       <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control :titles="['流行','新款','推荐']"></tab-control>
+      <ul>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+      </ul>
   </div>
 </template>
 
 <script>
-
-import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
 
-import { getHomeMultidata } from 'network/home.js'
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from "components/content/tabControl/TabControl"
+
+import { getHomeMultidata, getHomeGoods } from 'network/home.js'
 
 
 export default {
@@ -22,29 +80,66 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+    TabControl
   },
   data(){
     return{
       banners:[] ,
-      recommends:[]
-    }
+      recommends:[] ,
+      goods:{
+        new:{page:0 , list: []},
+        pop:{page:0 , list: []},
+        sell:{page:0 , list: []}
+      }
+    } 
   }
   ,
   created(){
-    getHomeMultidata()
-    .then(res =>{
+    this.getHomeMultidata()
+    this.getHomGoods('new',1)
+    this.getHomGoods('pop',1)
+    this.getHomGoods('sell',1)
+    
+  },
+  methods:{
+    getHomeMultidata(){
+       getHomeMultidata()
+      .then(res =>{
       this.banners = res.data.banner.list 
       this.recommends = res.data.recommend.list
     })
+    },
+    getHomGoods(type){
+      const page = this.goods[type].page + 1
+      getHomeGoods(type,page)
+      .then(res =>{
+        console.log(res)
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page +=1
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+    }
   }
 
 };
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+
 }
 </style>
